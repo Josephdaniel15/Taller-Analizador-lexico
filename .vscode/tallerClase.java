676d3e0c92cc344bd00bdb7e903f619f389f46d7
Josephdaniel15 <joseph.cruz01@epn.edu.ec>
2026-05-14 16:55:58 -0500

    probarque se envio

---
+++ .vscode/tallerClase.java
@@ -0,0 +1,26 @@
+public class tallerClase {
+    public static void main(String[] args) {
+        
+    }
+   public boolean esComentario(String palabraVer) {
+        // ver si está vacío o no tiene los caracteres necesarios
+        if (palabraVer == null || palabraVer.length() < 2) {
+            return false;
+        }
+        
+        // Verificar comentario normal
+        if (palabraVer.charAt(0) == '/' && palabraVer.charAt(1) == '/') {
+            return true;
+        }
+        
+        // Verificar comentario largo cuando son varias lineas /* */
+        if (palabraVer.charAt(0) == '/' && palabraVer.charAt(1) == '*') {
+            if (palabraVer.length() >= 4 && palabraVer.charAt(palabraVer.length() - 2) == '*' && 
+            palabraVer.charAt(palabraVer.length() - 1) == '/') {
+                return true; 
+            }
+        }
+        
+        return false;
+    }
+}
